a9848cbc39172b2f0cf1cef02391c30a7a02b421
Gonzalo <huntymonster@gmail.com>
2023-09-20 18:01:44 +0200

    movie mock

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import Logo from "../assets/images/logo.png";
+import movies from "../assets/data/movies.json";
 
 export const MovieList = () => {
-  return <div>MovieList</div>;
+  return (
+    <div className="h-screen w-screen">
+      <div className="fixed top-0 flex justify-center lg:justify-left px-4 bg-gray-600 h-2/12 w-full">
+        <img src={Logo} className="w-24 md:w-36" />
+        <h1>Must Watch!</h1>
+      </div>
+      <div className="flex justify-center items-center h-10/12 w-full overflow-y-scroll"></div>
+    </div>
+  );
 };
